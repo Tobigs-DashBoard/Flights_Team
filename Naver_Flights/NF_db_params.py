e8f34bbf32fbf21bccc89672d4395e6707ace764
9unu <gw.park@khu.ac.kr>
2024-08-21 07:35:22 +0900

    [REMOVE]params

--- Naver_Flights/NF_db_params.py
+++ Naver_Flights/NF_db_params.py
@@ -1,12 +1,13 @@
 import psycopg2
 from NF_functions import logger
-# DB 연결 함수
+
+
 def get_db_connection():
     return psycopg2.connect(
-        host="naver-flights.cdyk2yq24sjs.ap-northeast-2.rds.amazonaws.com", # 인스턴스 엔드포인트
+        host="localhost", # 인스턴스 엔드포인트
         database="naver_db", # 데이터베이스 이름
         user="gunu", # 사용자 이름
-        password="rjsdn5994!" # 사용자 비밀번호
+        password="5994" # 사용자 비밀번호
     )
 
 # DB 쿼리 실행 함수
@@ -22,19 +23,19 @@ def execute_db_query(conn,cur, query, params=None):
     
 
 query_dict={
-    "flights_table":
+    "layover_info":
             """
-            INSERT INTO flights (air_id, is_domestic, is_layover, layover_list, fetched_date)
-            VALUES (%s, %s, %s, %s, %s)
+            INSERT INTO layover_info (air_id, segment_id, connect_time, fetched_date)
+            VALUES (%s, %s, %s, %s)
             ON CONFLICT (air_id, fetched_date) DO UPDATE
-            SET is_layover = EXCLUDED.is_layover
+            SET connect_time = EXCLUDED.connect_time
             """,
 
-    "flight_info_table":
+    "flight_info":
             """
             INSERT INTO flight_info
             (air_id, airline, depart_country, depart_airport, depart_timestamp, arrival_country, 
-            arrival_airport, arrival_timestamp, journey_time, connect_time, fetched_date)
+            arrival_airport, arrival_timestamp, journey_time, fetched_date)
             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
             ON CONFLICT (air_id, fetched_date) DO UPDATE
             SET airline = EXCLUDED.airline,
@@ -43,12 +44,11 @@ query_dict={
                 arrival_country = EXCLUDED.arrival_country,
                 arrival_airport = EXCLUDED.arrival_airport,
                 journey_time = EXCLUDED.journey_time,
-                connect_time = EXCLUDED.connect_time,
                 depart_timestamp = EXCLUDED.depart_timestamp,
                 arrival_timestamp = EXCLUDED.arrival_timestamp
             """,
     
-    "fare_info_table":
+    "fare_info":
             """
             INSERT INTO fare_info 
             (air_id, option_type, agt_code, adult_fare, child_fare, infant_fare, purchase_url, fetched_date)
